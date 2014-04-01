da61458e0dc66fc7e79f083650c64b5c436578a0
vasnake <vasnake@gmail.com>
2014-04-01 20:29:32 +0400

    added: another computeCost test

--- ex1/test_computeCost.m
+++ ex1/test_computeCost.m
@@ -5,4 +5,8 @@ function test_computeCost ()
     a = 7.0175;
     assert(x, a, epsilon);
 
+    x = computeCost( [1 2; 1 3; 1 4; 1 5], [7;6;5;4], [0.1;0.2] );
+    a =  11.9450;
+    assert(x, a, epsilon);
+
 endfunction

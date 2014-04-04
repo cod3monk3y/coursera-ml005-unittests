516a6e634795bfe45f031cf2640906cb5907e258
vasnake <vasnake@gmail.com>
2014-04-04 20:50:35 +0400

    added: oneVsAll tests

--- ex3/test_ex3.m
+++ ex3/test_ex3.m
@@ -2,6 +2,10 @@
 
 %!test test_lrCostFunction()
 
+%!test test_oneVsAll_converged()
+
+% Beware! Results of the fmincg may vary!
+% You can try fminunc or a bigger number of iterations.
 %!test test_oneVsAll()
 
 %!test test_predictOneVsAll()
